42455d4e0c04da56462417c8b2484b5b3203b88f
HADY BHAI <118050034+hadybhai@users.noreply.github.com>
2022-12-26 15:41:53 +0500

    Create 7th day code challenge.py

---
+++ 7th day code challenge.py
@@ -0,0 +1,22 @@
+# exercise 1
+# calculator
+a = 50
+b = 3
+
+print("The value of", a, "+", 3, "is: ", a + b)
+print("The value of", a, "-", 3, "is: ", a - b)
+print("The value of", a, "*", 3, "is: ", a * b)
+print("The value of", a, "/", 3, "is: ", a / b)
+
+# ai = input("enter f n: ")
+# ba = input("enter op n: ")
+# cl = input("enter s n: ")
+
+# if ba == "+":
+#   print(int(ai) + int(cl))
+# if ba == "-":
+#   print(int(ai) - int(cl))
+# if ba == "*":
+#   print(int(ai) * int(cl))
+# if ba == "/":
+#   print(int(ai) / int(cl))
