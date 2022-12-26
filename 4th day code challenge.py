3316e5649c2f2c35ff0575a360ec26f793aece7b
HADY BHAI <118050034+hadybhai@users.noreply.github.com>
2022-12-26 15:24:50 +0500

    Update 4th day code challenge.py

--- 4th day code challenge.py
+++ 4th day code challenge.py
@@ -70,5 +70,31 @@
 # 3. end='end': Specify what to print at the end. Default is '\n' (line feed)
 # 4. file: An object with a write method. Default is sys.stdout
 # Parameters 2 to 4 are optional
+# code
+# comments and escape sequences
+#This is a 'Single-Line Comment'
+print("This is a print statement.")
+
+print("Hello World !!!")  #Printing Hello World
+
+#It will execute a block of code if a specified condition is true.
+#If the condition is false then it will execute another block of code.
+p = 7
+if (p > 5):
+  print("p is greater than 5.")
+else:
+  print("p is not greater than 5.")
+"""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code."""
+p = 7
+if (p > 5):
+  print("p is greater than 5.")
+else:
+  print("p is not greater than 5.")
+
+print('This doesnt "execute"')
+print("This will \" execute")
+
 
 
