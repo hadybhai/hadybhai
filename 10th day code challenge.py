bb7d026f37cdbd229319e34ee447df406938dd53
HADY BHAI <118050034+hadybhai@users.noreply.github.com>
2023-01-19 18:01:24 +0500

    Update 10th day code challenge.py

--- 10th day code challenge.py
+++ 10th day code challenge.py
@@ -74,3 +74,24 @@ for character in name:
     print(character)
 # Above code prints all the characters in the string name one by one!
 
+# string
+name = "Harry"
+friend = "Rohan"
+anotherFriend = 'Lovish'
+apple = '''He said, 
+Hi Harry
+hey I am good
+"I want to eat an apple'''
+
+print("Hello, " + name)
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5]) # Throws an error
+print("Lets use a for loop\n")
+for character in apple:
+  print(character)
+
